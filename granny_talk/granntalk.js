b60fa8f8cfe61f09eaab623db2bd1a833acf516c
Vanessa.Dohnt <vanessa.e.dohnt@gmail.com>
2017-06-05 11:09:33 +1000

    attempt at warmup

---
+++ granny_talk/granntalk.js
@@ -0,0 +1,30 @@
+var normal = "SPEAK UP SONNY JIM";
+var allCaps = "NO, NOT SINCE";
+var year = "1930";
+var bye = "What's that honey, I didn't hear you..";
+var input = prompt('grannyTalk');
+
+function grannyTalk(phrase) {
+  if (phrase.toLowerCase() === "bye") {
+    console.log(bye);
+  }else if (phrase.toUpperCase() ) {
+
+  }
+  console.log(normal);
+}
+//
+// if (input === normal.toLowerCase) {
+//   console.log(normal);
+// }
+//
+// else if (input === allCaps.toUpperCase) {
+//   console.log(allCaps + year);
+// }
+//
+// else if (input === bye) {
+//   console.log(bye);
+// }
+
+// toLowerCase {}
+//
+// toUpperCase {}
