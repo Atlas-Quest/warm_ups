47465c8f7d3c190cff02a4d049cac6461b89c52c
Vanessa Dohnt <vanessadohnt@Vanessas-MacBook-Pro.local>
2017-09-12 18:20:49 +1000

    updated

---
+++ roman_numerals/roman_numerals.js
@@ -0,0 +1,8 @@
+var numerals = [{ i: 1, v:5, x: 10, l: 50, c: 100, d: 500, m: 1000 }]
+
+var calulateNumerals = function(){
+  for (var i = 0; i < numerals.length; i++) {
+    
+    console.log(numerals)
+  }
+}
---
+++ split_bill/split.js
@@ -0,0 +1,9 @@
+var group = {Gavin: 20, Susan: 15, Luke: 10}
+var bill = 20;
+
+
+var splitBill = function(group) {
+  for (var i = 0; i < group.length; i++) {
+    return group[i];
+  }
+}
